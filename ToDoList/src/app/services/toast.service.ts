6fb46a87edd21d7ac2b1d98c94916e58b579ec86
KitBlagov <115415701+KitBlagov@users.noreply.github.com>
2023-11-23 19:49:05 +0300

    Remade deleteSchedule

--- ToDoList/src/app/services/toast.service.ts
+++ ToDoList/src/app/services/toast.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
@@ -11,23 +9,27 @@ export class ToastService {
   private stateToasts: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
   public toasts$: Observable<string[]> = this.stateToasts.asObservable();
 
-  constructor() {
-    setInterval(() => {
-      this.deleteLastToast();
-    }, 2000);
-  }
-
   public showToast(message: string): void {
    const lastStateToasts = this.stateToasts.value;
 
+   this.scheduleDeletion(message);
    this.stateToasts.next([...lastStateToasts, message]);
   }
 
-  private deleteLastToast(): void {
-    const lastStateToasts = this.stateToasts.value;
+  private scheduleDeletion(message: string, timeSchedule: number = 2000): void {
+    setTimeout(() => {
+      const lastStateToasts = this.stateToasts.value;
 
-    lastStateToasts.pop();
-    this.stateToasts.next(lastStateToasts);
+      for (let i = 0; i < lastStateToasts.length; i++) {
+        if (lastStateToasts[i] === message) {
+          lastStateToasts.splice(i, 1);
+          break;
+        }
+      }
+
+      this.stateToasts.next(lastStateToasts);
+
+    }, timeSchedule);
   }
 
 }
